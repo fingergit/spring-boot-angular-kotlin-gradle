efe1a8547249cbe3009f251623ac6f88d3cd0f84
redos <>
2018-10-28 12:40:53 +0800

    Add /resource endpoint

--- angular-demo/src/app/app.component.ts
+++ angular-demo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-demo';
+
+  data = {};
+  constructor (private httpClient: HttpClient) {
+    httpClient.get("/resource").subscribe(
+      data =>
+        this.data = data
+    )
+  }
 }
--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,9 +2,21 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class DemoApplication
+@RestController
+class DemoApplication {
+
+    @GetMapping("/resource")
+    fun resource(): Map<String, Any> {
+        return hashMapOf(
+                Pair("name", "Tony"),
+                Pair("age", 30)
+        )
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
